8b838f8b38724782c76e243640b9dcaaab05c105
Loïc Comeliau <loic.comeliau@gmail.com>
2021-10-27 18:21:32 +0200

    cv for each method

--- code/cross_validation.py
+++ code/cross_validation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from proj1_helpers import compute_loss
+from proj1_helpers import compute_loss, accuracy
 
 from implementations import least_squares_GD, least_squares_SGD, \
     ridge_regression, logistic_regression, reg_logistic_regression
@@ -18,7 +18,43 @@ def build_k_indices(y, k_fold, seed):
     return np.array(k_indices)
 
 
-def cross_validation(y, tx, k_indices, k, lambda_):
+def cv_GD(y, tx, k_indices, k, max_iters, gamma):
+    """return the loss of ridge regression."""
+    initial_w = np.ones(len(tx[0])) * 0.1
+    # get k'th subgroup in test, others in train
+    tx_tr = np.delete(tx, k_indices[k], axis=0)
+    y_tr = np.delete(y, k_indices[k])
+    tx_te = tx[k_indices[k]]
+    y_te = y[k_indices[k]]
+    # choose desired method
+    weights, _ = least_squares_GD(y, tx, initial_w, max_iters, gamma)
+    # calculate the loss for train and test data
+    loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weights))
+    loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, weights))
+    # calculate accuracy
+    acc = accuracy(tx_tr, y_tr, tx_te, y_te, weights, print_=False)
+    return loss_tr, loss_te, acc
+
+
+def cv_SGD(y, tx, k_indices, k, max_iters, gamma):
+    """return the loss of ridge regression."""
+    initial_w = np.ones(len(tx[0])) * 0.1
+    # get k'th subgroup in test, others in train
+    tx_tr = np.delete(tx, k_indices[k], axis=0)
+    y_tr = np.delete(y, k_indices[k])
+    tx_te = tx[k_indices[k]]
+    y_te = y[k_indices[k]]
+    # choose desired method
+    weights, _ = least_squares_SGD(y, tx, initial_w, max_iters, gamma)
+    # calculate the loss for train and test data
+    loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weights))
+    loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, weights))
+    # calculate accuracy
+    acc = accuracy(tx_tr, y_tr, tx_te, y_te, weights, print_=False)
+    return loss_tr, loss_te, acc
+
+
+def cv_ridge_regression(y, tx, k_indices, k, lambda_):
     """return the loss of ridge regression."""
     # get k'th subgroup in test, others in train
     tx_tr = np.delete(tx, k_indices[k], axis=0)
@@ -30,7 +66,45 @@ def cross_validation(y, tx, k_indices, k, lambda_):
     # calculate the loss for train and test data
     loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weights))
     loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, weights))
-    return loss_tr, loss_te
+    # calculate accuracy
+    acc = accuracy(tx_tr, y_tr, tx_te, y_te, weights, print_=False)
+    return loss_tr, loss_te, acc
+
+
+def cv_logistic_regression(y, tx, k_indices, k, max_iters, gamma):
+    """return the loss of ridge regression."""
+    initial_w = np.ones(len(tx[0])) * 0.1
+    # get k'th subgroup in test, others in train
+    tx_tr = np.delete(tx, k_indices[k], axis=0)
+    y_tr = np.delete(y, k_indices[k])
+    tx_te = tx[k_indices[k]]
+    y_te = y[k_indices[k]]
+    # choose desired method
+    weights, _ = logistic_regression(y, tx, initial_w, max_iters, gamma)
+    # calculate the loss for train and test data
+    loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weights))
+    loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, weights))
+    # calculate accuracy
+    acc = accuracy(tx_tr, y_tr, tx_te, y_te, weights, print_=False)
+    return loss_tr, loss_te, acc
+
+
+def cv_reg_logistic_regression(y, tx, k_indices, k, lambda_, max_iters, gamma):
+    """return the loss of ridge regression."""
+    initial_w = np.ones(len(tx[0])) * 0.1
+    # get k'th subgroup in test, others in train
+    tx_tr = np.delete(tx, k_indices[k], axis=0)
+    y_tr = np.delete(y, k_indices[k])
+    tx_te = tx[k_indices[k]]
+    y_te = y[k_indices[k]]
+    # choose desired method
+    weights, _ = reg_logistic_regression(y, tx, initial_w, lambda_, max_iters, gamma)
+    # calculate the loss for train and test data
+    loss_tr = np.sqrt(2 * compute_loss(y_tr, tx_tr, weights))
+    loss_te = np.sqrt(2 * compute_loss(y_te, tx_te, weights))
+    # calculate accuracy
+    acc = accuracy(tx_tr, y_tr, tx_te, y_te, weights, print_=False)
+    return loss_tr, loss_te, acc
 
 
 def cross_validation_lambda_visualization(lambds, mse_tr, mse_te):
