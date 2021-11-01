6b836c93d39e033108e98af8eb01ebda7aeac11b
Loïc Comeliau <loic.comeliau@gmail.com>
2021-11-01 12:16:07 +0100

    cosmetics

--- code/run.py
+++ code/run.py
@@ -1,13 +1,11 @@
 import zipfile
 
-#import numpy as np
-
 from implementations import least_squares
 from data_processing import preprocessing
 from proj1_helpers import *
 from cross_validation import build_k_indices, cv_polynomial_expansion
 
-# Load train and test data
+"""Load train and test data"""
 DATA_TRAIN_PATH = '../data/train.csv'
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 zf = zipfile.ZipFile('../data/test.csv.zip')
@@ -15,18 +13,20 @@ zf.extract('test.csv', path='../data')
 DATA_TEST_PATH = '../data/test.csv'
 y_test, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
 
-# Remove non-used features (Feature processing)
+"""Feature processing"""
+# Remove non-used features
 removed_indices_missing = [4, 5, 6, 12, 26, 27, 28]
 removed_indices_univariate = [15, 18, 20, 25]
 removed_indices_covariate = [9, 29]
 removed_indices = removed_indices_univariate + removed_indices_covariate + removed_indices_missing
+
 tX = np.delete(tX, removed_indices, 1)
 tX_test = np.delete(tX_test, removed_indices, 1)
 
 # Feature preprocessing (split, missing values replacement, standardisation and model building)
 train_tx, train_y, val_tx, val_y, test_tx, test_y = preprocessing(tX, y, tX_test, y_test, method='mean', ratio=0.7)
 
-# Polynomial feature expansion (cross validation)
+"""Polynomial feature expansion (using cross validation)"""
 # Cross validation parameter setting
 seed = 1
 k_fold = 5
@@ -64,15 +64,15 @@ best_degrees = []
 for i in features:
     best_degrees.append(np.argmax(acc_te[i]))
 
-# Compute expanded datasets
+"""Compute expanded datasets"""
 expanded_train_tx = build_final_poly_tx(train_tx, best_degrees)
 expanded_val_tx = build_final_poly_tx(val_tx, best_degrees)
 expanded_test_tx = build_final_poly_tx(test_tx, best_degrees)
 
-# Compute model
+"""Compute model"""
 expanded_weights, expanded_loss = least_squares(train_y, expanded_train_tx)
 
-# Write in output file
+"""Write in output file"""
 OUTPUT_PATH = '../data/sample-submission.csv'
 y_pred = predict_labels(expanded_weights, expanded_test_tx)
 y_pred[y_pred == 0] = -1
